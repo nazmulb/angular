299604a12e4d0349dcb76ba6d95d8d66a3b53010
nazmulbasher <nazmul.basher@fieldnation.com>
2020-06-08 16:48:54 +0300

    added codes

--- learning/angular-material-scss/src/app/app.component.ts
+++ learning/angular-material-scss/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Angular Material Scss';
+  genderList: string[] = ['Man', 'Woman'];
+  form = new FormGroup({
+    fname: new FormControl('', Validators.required),
+    gender: new FormControl([], [Validators.required]),
+  });
+
+  actionSave() {
+    console.log(this.form.controls["fname"].value, this.form.controls["gender"].value)
+  }
 }
--- learning/angular-material-scss/src/app/app.module.ts
+++ learning/angular-material-scss/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
